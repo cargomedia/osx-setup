8e4fbe80a4958df47d1356456f0a6b59cbe8e769
Reto Kaiser <reto@cargomedia.ch>
2014-03-11 09:55:04 +0100

    Install vagrant-dns plugin

---
+++ deploy/resource/osx/install/_default/scripts/vagrant-plugins.sh
@@ -0,0 +1,14 @@
+VAGRANT='vagrant'
+
+function installVagrantPlugin {
+  if (which $VAGRANT >/dev/null); then
+    if ! ($VAGRANT plugin list | grep -q $1); then
+      $VAGRANT plugin install $1
+    fi
+  fi
+}
+
+installVagrantPlugin vagrant-librarian-puppet
+installVagrantPlugin vagrant-phpstorm-tunnel
+installVagrantPlugin vagrant-vbox-snapshot
+installVagrantPlugin vagrant-dns

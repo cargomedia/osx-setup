965d4b2e673afadfe855ab4641634f6d51a0eb48
Reto Kaiser <reto@cargomedia.ch>
2014-04-14 13:40:47 +0200

    Formatting

--- deploy/library/Task/Sys/Packages.php
+++ deploy/library/Task/Sys/Packages.php
@@ -8,8 +8,12 @@ class Task_Sys_Packages extends Task {
 
     protected function _getDependencies() {
         if (Role::OS_OSX == $this->getRole()->getOperatingsystem()) {
-            return array(new Task_Sys_ScriptsInstall('brew.sh'), new Task_Sys_BrewUpgrade(), new Task_Sys_BrewCaskInstall(),
-                new Task_Sys_BrewInstall());
+            return array(
+                new Task_Sys_ScriptsInstall('brew.sh'),
+                new Task_Sys_BrewUpgrade(),
+                new Task_Sys_BrewCaskInstall(),
+                new Task_Sys_BrewInstall(),
+            );
         }
         return array();
     }

7ec57e23dfff04bd7e8baeea5f0e2066e8a9019a
Reto Kaiser <reto@cargomedia.ch>
2014-08-22 16:55:24 +0200

    Install nodejs and browser-sync

---
+++ deploy/resource/osx/install/_default/scripts/nodejs.sh
@@ -0,0 +1,9 @@
+NPM='/usr/local/bin/npm'
+
+function npmInstall {
+	if ! (${NPM} list -g "${1}" | grep -q "${1}@${2}"); then
+		${NPM} install -g "${1}@${2}"
+	fi
+}
+
+npmInstall browser-sync 1.3.6

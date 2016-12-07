3a98790651320021796d99cce123a51311d902ff
Reto Kaiser <reto@cargomedia.ch>
2016-12-07 10:06:14 +0100

    Remove 1pw

--- resource/osx/install/_default/scripts/osx.sh
+++ resource/osx/install/_default/scripts/osx.sh
@@ -19,7 +19,6 @@ if ! (test -e ~/.cargomedia-desktop); then
 	dockutil --no-restart --add '/Applications/Google Chrome.app'
 	dockutil --no-restart --add '/Applications/VirtualBox.app'
 	dockutil --no-restart --add '/Applications/Xcode.app/Contents/Developer/Applications/iOS Simulator.app'
-	dockutil --no-restart --add '/Applications/1Password 5.app'
 	dockutil --no-restart --add '/Applications/TextEdit.app'
 	dockutil --no-restart --add '/Applications/Utilities/Terminal.app'
 	dockutil --no-restart --add '/Applications/PhpStorm.app'

7c8e4c968c79c2592a9a793b636e2875affdebbe
Reto Kaiser <reto@cargomedia.ch>
2017-01-25 15:57:42 +0100

    Manually install xcode
    Seems that expect script doesn't work anymore (reported by Aleksei)

--- install.php
+++ install.php
@@ -8,7 +8,6 @@ $dep->addRole('default', 'osx')->addHost('127.0.0.1', $hostname);
 
 $taskSysInstall = new Task_Sys_Install();
 $taskSysInstall->depends(
-	new Task_Sys_ScriptsInstall('xcode.sh'),
 	new Task_Sys_Packages(),
 	new Task_Sys_Files(),
 	new Task_Sys_ScriptsInstall()
